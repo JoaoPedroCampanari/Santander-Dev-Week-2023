610fc8b36ed75bb92a8dcb64d9f8fbd84948177e
Joaofcampanari <joaofcampanari@gmail.com>
2024-06-22 13:41:34 -0300

    Pacotes services e controller criados

---
+++ src/main/java/me/dio/service/impl/UserServerImpl.java
@@ -0,0 +1,38 @@
+package me.dio.service.impl;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServerImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServerImpl (UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userToCreate.getId() != null && userRepository.existsById(userToCreate.getId())){
+            throw new IllegalArgumentException("This Account number alredy exists!");
+        }
+        else if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())){
+            throw new IllegalArgumentException("This user ID alredy exists!");
+        }
+
+        return userRepository.save(userToCreate);
+
+    }
+}
